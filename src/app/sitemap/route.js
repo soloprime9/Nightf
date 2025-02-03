c9fe56601f5b02da939b9057bd77f13f43e5773f
soloprime9 <primeindia5750@gmail.com>
2025-02-04 01:22:25 +0530

    Update route.js

--- src/app/sitemap/route.js
+++ src/app/sitemap/route.js
@@ -1,7 +1,19 @@
-const nextConfig = {
-  experimental: {
-    externalDir: true,  // Keep this if needed
-  },
-};
+export async function GET() {
+  const siteUrl = "https://www.okhatrimaza.shop";
+  const staticPages = [
+    `${siteUrl}/`,
+    `${siteUrl}/about`,
+    `${siteUrl}/contact`,
+  ];
 
-export default nextConfig;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticPages.map((url) => `<url><loc>${url}</loc></url>`).join("")}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
