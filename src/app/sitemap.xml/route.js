59d0e7325220886e125ef4c1345656b8ac5fbb22
soloprime9 <primeindia5750@gmail.com>
2025-02-04 00:56:52 +0530

    Update route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,31 +1,17 @@
 export async function GET() {
   const siteUrl = "https://www.okhatrimaza.shop";
-
   const staticPages = [
-    "/",
-    "/about",
-    "/contact",
-    "/privacy-policy",
+    `${siteUrl}/`,
+    `${siteUrl}/about`,
+    `${siteUrl}/contact`
   ];
 
-  const lastModifiedDate = new Date().toISOString(); // Dynamic last modified date
-
-  // Generate XML content
-  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticPages
-      .map(
-        (page) => `
-      <url>
-        <loc>${siteUrl}${page}</loc>
-        <lastmod>${lastModifiedDate}</lastmod>
-        <priority>${page === "/" ? "1.0" : "0.8"}</priority>
-      </url>`
-      )
-      .join("")}
+    ${staticPages.map((url) => `<url><loc>${url}</loc></url>`).join("")}
   </urlset>`;
 
-  return new Response(sitemapXml, {
+  return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
     },
