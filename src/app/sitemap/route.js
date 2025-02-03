9bbbd308edcbcca5cb85e70edcb869ed1fea6d72
soloprime9 <primeindia5750@gmail.com>
2025-02-04 02:13:23 +0530

    Update route.js

--- src/app/sitemap/route.js
+++ src/app/sitemap/route.js
@@ -1,15 +1,32 @@
-import { NextResponse } from 'next/server';
+import { GetServerSideProps } from 'next';
 
-export async function GET() {
-  const url = 'https://example.com'; // Apne website ka URL daalo
+const Sitemap = () => null;
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${url}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-    </url>
-  </urlset>`;
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const routes = [
+    { url: '/', changefreq: 'daily', priority: 1.0 },
+    { url: '/about', changefreq: 'monthly', priority: 0.5 },
+  ];
 
-  return NextResponse.json({ sitemap });
-}
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${routes.map((route) => `
+        <url>
+          <loc>${route.url}</loc>
+          <changefreq>${route.changefreq}</changefreq>
+          <priority>${route.priority}</priority>
+        </url>
+      `).join('')}
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemapXml);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
