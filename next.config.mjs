ae3972bf4ba1c0716f1d7fd5cbf60db5af7205e2
soloprime9 <primeindia5750@gmail.com>
2025-02-04 02:20:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 export default {
   webpack(config) {
-    // Ensure babel-loader is included for transpiling JS files
     config.module.rules.push({
       test: /\.js$/,
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env', 'next/babel'],
       },
+      exclude: /node_modules/,
     });
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
     return config;
   },
 };
