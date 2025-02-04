12488bc00e35abb9ffded8ceaf8057db3e56ec5e
soloprime9 <primeindia5750@gmail.com>
2025-02-04 11:57:13 +0530

    Update route.js

--- src/app/sitemap/route.js
+++ src/app/sitemap/route.js
@@ -1,6 +1,3 @@
-import { GetServerSideProps } from 'next';
-
-const Sitemap = () => null;
 export const GET = async () => {
   const url = 'https://example.com'; // Apne website ka URL daalo
 
@@ -18,32 +15,3 @@ export const GET = async () => {
     },
   });
 };
-
-export async function getServerSideProps({ res }) {
-  const routes = [
-    { url: '/', changefreq: 'daily', priority: 1.0 },
-    { url: '/about', changefreq: 'monthly', priority: 0.5 },
-  ];
-
-  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${routes.map((route) => `
-        <url>
-          <loc>${route.url}</loc>
-          <changefreq>${route.changefreq}</changefreq>
-          <priority>${route.priority}</priority>
-        </url>
-      `).join('')}
-    </urlset>
-  `;
-
-  res.setHeader('Content-Type', 'text/xml');
-  res.write(sitemapXml);
-  res.end();
-
-  return {
-    props: {},
-  };
-}
-
-export default Sitemap;
