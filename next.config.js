4fa8c8d8ba66dec485347164ac424e2cbcf84cdf
soloprime9 <primeindia5750@gmail.com>
2025-02-04 17:36:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.js$/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', 'next/babel'],
-      },
-      exclude: /node_modules/,
-    });
-    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
-    config.output = { ...config.output, ...{ publicPath: '' } };
-    return config;
-  },
-  target: 'server',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone', // Ensure correct output mode
 };
+
+module.exports = nextConfig;
