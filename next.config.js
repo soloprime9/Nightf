92fa1fe40fa85bce7ac6f39b34644dff1a2e16cc
soloprime9 <primeindia5750@gmail.com>
2025-02-04 22:37:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone', // Ensure correct output mode
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Matches all unknown paths
+        destination: "/",
+        permanent: false, // Use `true` for a permanent 301 redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
