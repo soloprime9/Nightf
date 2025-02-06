26c68819da8da031454c1af82c2a9258b893f743
soloprime9 <primeindia5750@gmail.com>
2025-02-06 09:51:35 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,50 +2,33 @@ import React from 'react';
 
 function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Hello, Dear friends, Welcome to YouTube Thumbnail Downloader also, we are happy you want to know something more about our site</p>
-      <p>So, basically, nowadays people are more dependent on online products and services that's why we also, take forward a step to help you.</p>
-      <p>Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any solution then mention it in the comment section.</p>
-      <h2>What is Our Goal?</h2>
-      <p>There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
-      <p>So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better experience on the world wide web.</p>
-      <h3>What is our Service?</h3>
-      <p>We are mainly focused on the Online Tools & Utilities category so, we provide Online Tools & Utilities related content if you are interested in the Online Tools & Utilities category then you can visit daily to get more latest information.</p>
-      <p>YouTube Thumbnail Downloader service. Users can paste a YouTube video link into a provided box, click on the "Get Thumbnail" button, and then download high-quality thumbnails of their choice</p>
-      <h3>About YouTube Thumbnail Downloader</h3>
-      <p>As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Online Tools & Utilities Category to help people.</p>
-      <p>This Website is Created By YouTube Thumbnail Downloader to help people because many people are still spending hours of time to get exact information so, this is the only motive to create <a href="https://www.okhatrimaza.shop/">YouTube Thumbnail Downloader</a> to help people and provide them a better web experience.</p>
-      <h3>Admin's Statement for YouTube Thumbnail Downloader</h3>
-      <p>As per my point of view, there are many people who visit the internet to get some information but 90% of the time they get wrong information so, the first priority of our website YouTube Thumbnail Downloader is to provide 100% legit and accurate information to our users, Also, I hope my dream comes true one day, and our website will provide Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
-      <h3>Admin's Contact Information</h3>
-      <table>
+    <div className="max-w-2xl mx-auto p-4 md:p-6 lg:p-8 mt-10 mb-10 bg-white rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p className="text-lg text-gray-600 mb-6">Hello, Dear friends, Welcome to YouTube Thumbnail Downloader also, we are happy you want to know something more about our site</p>
+      <p className="text-lg text-gray-600 mb-6">So, basically, nowadays people are more dependent on online products and services that's why we also, take forward a step to help you.</p>
+      <p className="text-lg text-gray-600 mb-6">Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any solution then mention it in the comment section.</p>
+      <h2 className="text-2xl font-bold mb-2">What is Our Goal?</h2>
+      <p className="text-lg text-gray-600 mb-6">There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
+      <p className="text-lg text-gray-600 mb-6">So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better experience on the world wide web.</p>
+      <h3 className="text-xl font-bold mb-2">What is our Service?</h3>
+      <p className="text-lg text-gray-600 mb-6">We are mainly focused on the Online Tools & Utilities category so, we provide Online Tools & Utilities related content if you are interested in the Online Tools & Utilities category then you can visit daily to get more latest information.</p>
+      <p className="text-lg text-gray-600 mb-6">YouTube Thumbnail Downloader service. Users can paste a YouTube video link into a provided box, click on the "Get Thumbnail" button, and then download high-quality thumbnails of their choice</p>
+      <h3 className="text-xl font-bold mb-2">About YouTube Thumbnail Downloader</h3>
+      <p className="text-lg text-gray-600 mb-6">As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Online Tools & Utilities Category to help people.</p>
+      <p className="text-lg text-gray-600 mb-6">This Website is Created By YouTube Thumbnail Downloader to help people because many people are still spending hours of time to get exact information so, this is the only motive to create <a href="https://www.okhatrimaza.shop/" className="text-blue-600 hover:text-blue-800">YouTube Thumbnail Downloader</a> to help people and provide them a better web experience.</p>
+      <h3 className="text-xl font-bold mb-2">Admin's Statement for YouTube Thumbnail Downloader</h3>
+      <p className="text-lg text-gray-600 mb-6">As per my point of view, there are many people who visit the internet to get some information but 90% of the time they get wrong information so, the first priority of our website YouTube Thumbnail Downloader is to provide 100% legit and accurate information to our users, Also, I hope my dream comes true one day, and our website will provide Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
+      <h3 className="text-xl font-bold mb-2">Admin's Contact Information</h3>
+      <table className="table-auto w-full mb-6">
         <tbody>
           <tr>
-            <td>Name</td>
-            <td>Five</td>
+            <td className="px-4 py-2">Name</td>
+            <td className="px-4 py-2">Five</td>
           </tr>
           <tr>
-            <td>Email</td>
-            <td><a href="mailto:five@gmail.com">five@gmail.com</a></td>
+            <td className="px-4 py-2">Email</td>
+            <td className="px-4 py-2"><a href="mailto:five@gmail.com" className="text-blue-600 hover:text-blue-800">five@gmail.com</a></td>
           </tr>
           <tr>
-            <td>Facebook Id</td>
-            <td><a href="https://www.facebook.com/profile.php?id=100090486122290">https://www.facebook.com/profile.php?id=100090486122290</a></td>
-          </tr>
-          <tr>
-            <td>Twitter Id</td>
-            <td><a href="https://www.facebook.com/profile.php?id=100090486122290">https://www.facebook.com/profile.php?id=100090486122290</a></td>
-          </tr>
-          <tr>
-            <td>Instagram Id</td>
-            <td><a href="https://www.facebook.com/profile.php?id=100090486122290">https://www.facebook.com/profile.php?id=100090486122290</a></td>
-          </tr>
-        </tbody>
-      </table>
-      <p>Thanks for visiting our About Us Page</p>
-    </div>
-  );
-}
-
-export default AboutUs;
+            <td className="px-4 py-2">Facebook Id</td>
+            <td className="px-4 py-2"><a href="https://www.facebook.com/profile.php?id=100090486122290" className="text-blue-600 hover:text-blue-800">https://www.facebook.com/profile.php?id=100090486122290</a></td>
