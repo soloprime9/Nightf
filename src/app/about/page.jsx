852bfd7b5c710acb983679c3dc275d86154e4b69
soloprime9 <primeindia5750@gmail.com>
2025-02-06 10:10:34 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
-
-function AboutUs() {
-  return (
-    <div className="max-w-2xl mx-auto p-4 md:p-6 lg:p-8 mt-10 mb-10 bg-white rounded-lg shadow-md">
-      <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="text-lg text-gray-600 mb-6">Hello, Dear friends, Welcome to YouTube Thumbnail Downloader also, we are happy you want to know something more about our site</p>
-      <p className="text-lg text-gray-600 mb-6">So, basically, nowadays people are more dependent on online products and services that's why we also, take forward a step to help you.</p>
-      <p className="text-lg text-gray-600 mb-6">Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any solution then mention it in the comment section.</p>
-      <h2 className="text-2xl font-bold mb-2">What is Our Goal?</h2>
-      <p className="text-lg text-gray-600 mb-6">There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
-      <p className="text-lg text-gray-600 mb-6">So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better experience on the world wide web.</p>
-      <h3 className="text-xl font-bold mb-2">What is our Service?</h3>
-      <p className="text-lg text-gray-600 mb-6">We are mainly focused on the Online Tools & Utilities category so, we provide Online Tools & Utilities related content if you are interested in the Online Tools & Utilities category then you can visit daily to get more latest information.</p>
-      <p className="text-lg text-gray-600 mb-6">YouTube Thumbnail Downloader service. Users can paste a YouTube video link into a provided box, click on the "Get Thumbnail" button, and then download high-quality thumbnails of their choice</p>
-      <h3 className="text-xl font-bold mb-2">About YouTube Thumbnail Downloader</h3>
-      <p className="text-lg text-gray-600 mb-6">As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Online Tools & Utilities Category to help people.</p>
-      <p className="text-lg text-gray-600 mb-6">This Website is Created By YouTube Thumbnail Downloader to help people because many people are still spending hours of time to get exact information so, this is the only motive to create <a href="https://www.okhatrimaza.shop/" className="text-blue-600 hover:text-blue-800">YouTube Thumbnail Downloader</a> to help people and provide them a better web experience.</p>
-      <h3 className="text-xl font-bold mb-2">Admin's Statement for YouTube Thumbnail Downloader</h3>
-      <p className="text-lg text-gray-600 mb-6">As per my point of view, there are many people who visit the internet to get some information but 90% of the time they get wrong information so, the first priority of our website YouTube Thumbnail Downloader is to provide 100% legit and accurate information to our users, Also, I hope my dream comes true one day, and our website will provide Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
-      <h3 className="text-xl font-bold mb-2">Admin's Contact Information</h3>
-      <table className="table-auto w-full mb-6">
-        <tbody>
-          <tr>
-            <td className="px-4 py-2">Name</td>
-            <td className="px-4 py-2">Five</td>
-          </tr>
-          <tr>
-            <td className="px-4 py-2">Email</td>
-            <td className="px-4 py-2"><a href="mailto:five@gmail.com" className="text-blue-600 hover:text-blue-800">five@gmail.com</a></td>
-          </tr>
-          <tr>
-            <td className="px-4 py-2">Facebook Id</td>
-            <td className="px-4 py-2"><a href="https://www.facebook.com/profile.php?id=100090486122290" className="text-blue-600 hover:text-blue-800">https://www.facebook.com/profile.php?id=100090486122290</a></td>
+var MyClass = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Hello, Dear friends, Welcome to YouTube Thumbnail Downloader also, we are happy you want to know something more about our site</p>
+        <p>So, basically, nowadays people are more dependent on online products and services that's why we also, take forward a step to help you.</p>
+        <p>Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any solution then mention it in the comment section.</p>
+        <p>Also, we are trying to provide fresh &amp; latest content that provides you ideas about all updated information that's happening in the world.</p>
+        <p>In the below section you can get more ideas about our site like our website category and content category.</p>
+        <p>If you have additional questions or require more information about our About Us Page, do not hesitate to contact us through email at <b>five@gmail.com</b></p>
+        <h2><b>What is Our Goal?</b></h2>
+        <p>There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
+        <p>So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better experience on the world wide web.</p>
+        <p>We mainly focus on our service so and improving it regularly to provide a better user experience to all users.</p>
+        <p>Basically, we focus on the Online Tools &amp; Utilities niche so, our main priority is to search for new content and present it in front of you to learn something new. </p>
+        <h3><b>What is our Service?</b></h3>
+        <p>We are mainly focused on the Online Tools &amp; Utilities category so, we provide Online Tools &amp; Utilities related content if you are interested in the Online Tools &amp; Utilities category then you can visit daily to get more latest information.</p>
+        <p>YouTube Thumbnail Downloader service. Users can paste a YouTube video link into a provided box, click on the "Get Thumbnail" button, and then download high-quality thumbnails of their choice</p>
+        <p>On our website YouTube Thumbnail Downloader you get can all Online Tools &amp; Utilities related information also, we focus on many other categories and we hope you like also, the content of other categories that are maintained on our website. So, you can visit our website homepage to know all category details here you can visit our homepage Click here --&gt;<a href"https: www.okhatrimaza.shop ">YouTube Thumbnail Downloader</a>.</p>
+        <p>Also, we provide a Notification update service you can join by email and other Social Media Platforms, and all Links you can get on the Homepage visit now. <a href"https: www.okhatrimaza.shop ">YouTube Thumbnail Downloader</a></p>
+        <h3><b>About YouTube Thumbnail Downloader</b></h3>
+        <p>As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Online Tools &amp; Utilities Category to help people.</p>
+        <p>This Website is Created By YouTube Thumbnail Downloader to help people because many people are still spending hours of time to get exact information so, this is the only motive to create <a href"https: www.okhatrimaza.shop ">YouTube Thumbnail Downloader</a> to help people and provide them a better web experience. Also, This About Us Page is Generated With <a href="https://soumyahelp.com">Soumya help</a>.</p>
+        <p>Now, the time is to know about the Admin Details of this website, so, now we have to go down to know about Admin details.</p>
+        <h3><b>Admin's Statement for YouTube Thumbnail Downloader</b></h3>
+        <p>As per my point of view, there are many people who visit the internet to get some information but 90% of the time they get wrong information so, the first priority of our website YouTube Thumbnail Downloader is to provide 100% legit and accurate information to our users, Also, I hope my dream comes true one day, and our website will provide Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
+        <h3><b>Admin's Contact Information</b></h3>
+        <p>Hi, this Five, in the above paragraph you know about the website properly and now I am going to provide my contact details.</p>
+        <p>If you have any problem &amp; suggestions for this website then you can contact me by using following contact details.</p>
+        <center>
+          <strong /><table><tbody><tr><td>Name</td><td>Five</td></tr><tr><td>Email</td><td><strong>five@gmail.com</strong></td></tr><tr><td>Facebook Id</td><td><a href="https://www.facebook.com/profile.php?id=100090486122290"><strong>https://www.facebook.com/profile.php?id=100090486122290</strong></a></td></tr><tr><td>Twitter Id</td><td><strong><a href="https://www.facebook.com/profile.php?id=100090486122290">https://www.facebook.com/profile.php?id=100090486122290</a></strong>/</td></tr><tr><td>Instagram Id</td><td><a href="https://www.facebook.com/profile.php?id=100090486122290"><strong>https://www.facebook.com/profile.php?id=100090486122290</strong></a></td></tr></tbody></table><strong>
+          </strong></center><strong>
+          <p>These details are my personal Account details if you want to contact me then you can contact me by the above platform.</p>
+          <p>Finally, this is our complete about us page about details are showing what is the motive to create YouTube Thumbnail Downloader. Also, this page is generated by Soumya Help <a href="https://soumyahelp.com/about-us-generator/">About Us Generator</a></p>
+          <p>If you want to contact us then you can email us at five@gmail.com also, you can contact us by our contact us form. Go to homepage --&gt; <a href="https://www.okhatrimaza.shop/">YouTube Thumbnail Downloader</a><a href="https://soumyahelp.com/about-us-generator/">.</a></p>
+          <center>
+            <p>"Thanks for visiting our About Us Page"</p>
+          </center>
+        </strong></div>
+    );
+  }
+});
